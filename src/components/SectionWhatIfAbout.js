a08fd008e4f80b31b730841e25062f7eb3526702
Adeardo Dora Harnanda <adetxt@live.com>
2020-07-07 10:48:32 +0700

    adjusting SectionWhatIfAbout

--- src/components/SectionWhatIfAbout.js
+++ src/components/SectionWhatIfAbout.js
@@ -5,21 +5,20 @@ import aboutvirus from '../assets/images/about-virus.png';
 function SectionWhatIfAbout() {
   return (
     <div className="bg-gradient-pink-to-white sec-about">
-      <div className="flex flex-col-reverse md:flex-row mt-12" >
-        <div className="md:w-3/4 items-center">
+      <div className="flex flex-col items-center md:flex-row relative mt-12" >
+        <div className="lg:w-3/6 items-center z-10">
           <img alt="about" src={ about } />
         </div>
-        <div className="md:w-2/3 items-center pr-12">
-          <p className="text-3xl text-left secondary-text-color capitalize mt-32">What Is Covid-19</p>
-          <p className="text-4xl font-semibold text-left primary-text-color capitalize">Coronavirus</p>
-          <p className="font-light text-left mt-4 capitalize">Corona viruses are a type of virus. There are many different kinds, and some cause disease. A newly identified type has caused a recent outbreak of respiratory illness now called COVID-19.Lauren Sauer, M.S., the director of operations with the Johns Hopkins Office of Critical Event Preparedness and Response</p>
-          <div className="mt-8 text-left">
+        <div className="lg:w-3/6 items-center text-center lg:text-left px-4 lg:px-24 z-10">
+          <h3 className="text-xl font-semibold secondary-text-color capitalize">What Is Covid-19</h3>
+          <h1 className="text-4xl font-semibold primary-text-color capitalize">Coronavirus</h1>
+          <p className="text-sm leading-loose text-gray-700 mt-4 capitalize">Corona viruses are a type of virus. There are many different kinds, and some cause disease. A newly identified type has caused a recent outbreak of respiratory illness now called COVID-19.Lauren Sauer, M.S., the director of operations with the Johns Hopkins Office of Critical Event Preparedness and Response</p>
+          <div className="mt-8">
             <button className="btn-rounded bg-red-200 capitalize btn-learn-more">Learn More</button>
           </div>
         </div>
-        <div className="md:w-1/5 items-center">
-          <img className="mt-48" alt="about-virus" src={ aboutvirus } />
-        </div>
+
+        <img className="absolute bottom-0 right-0 z-0 w-32 mb-10" alt="about-virus" src={ aboutvirus } />
       </div>
     </div>
   );
